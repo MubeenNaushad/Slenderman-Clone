2ea46ba9f559d12ffc46cb3cda1b07e49705bdc9
Mubeen <mubeennaushad@gmail.com>
2024-06-18 00:47:26 +0500

    Update mainMenu.cs

--- Assets/Main Menu/mainMenu.cs
+++ Assets/Main Menu/mainMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class mainMenu : MonoBehaviour
 {
     public GameObject menu, options, credits, loading;
